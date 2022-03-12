b6537afb6c7af093e6e82da48e7e9a0ef542c6f3
ShakedSabo <98387385+ShakedSabo@users.noreply.github.com>
2022-03-12 21:28:13 +0200

    fixing

--- Score.py
+++ Score.py
@@ -2,17 +2,17 @@
 def add_score(difficulty):
     POINTS_OF_WINNING = str((int(difficulty) * 3) + 5)
     try:
-        score_file = open("Scores.txt", "r")
+        score_file = open(scores_file_name, "r")
         old_score = score_file.read()
         score_file.close()
-        score_file = open("Scores.txt", "w")
+        score_file = open(scores_file_name, "w")
         new_score = int(old_score)+int(POINTS_OF_WINNING)
         print("old = "+old_score)
         print("new = "+str(new_score))
         score_file.write(str(new_score))
         score_file.close()
     except FileNotFoundError:
-        score = open("Scores.txt", "x")
+        score = open(scores_file_name, "x")
         score.write(POINTS_OF_WINNING)
 
 
